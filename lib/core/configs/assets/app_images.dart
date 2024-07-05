ff8ad03261f68872cacf288c7ce20798a1164e63
niyal jacob <niyaljacob76@gmail.com>
2024-07-05 22:36:34 +0530

    first commit

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,8 @@
+class AppImages{
+  static const String basePath='assets/image/';
+  static const String introBG='${basePath}intro_bg.png';
+    // static const String chooseModeBG='$basePath choose_mode_bg.png';
+  static const String chooseModeBG='${basePath}intro_bg.png';
+
+
+}
