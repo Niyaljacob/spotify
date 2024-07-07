64e37bd679d85377f4e57be8c78fd370361725d2
niyal jacob <niyaljacob76@gmail.com>
2024-07-07 20:56:29 +0530

    login or signup page

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -4,6 +4,7 @@ class AppImages{
     // static const String chooseModeBG='$basePath choose_mode_bg.png';
   static const String chooseModeBG='${basePath}choose_mode_bg.png';
   static const String chooseModeMoon='${basePath}moon.png';
+  static const String authBG='${basePath}auth_bg.png';
 
 
 }
